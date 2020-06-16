3901d5a455549784290c843b6521337ccc5b5677
Jules Pietri <heahdude@yahoo.fr>
2020-06-16 15:39:52 +0200

    webpack encore

---
+++ assets/js/app.js
@@ -0,0 +1,17 @@
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+ */
+
+// any CSS you import will output into a single css file (app.css in this case)
+
+require('bootstrap');
+require('fontawesome');
+require('../css/sensiotv-theme.scss')
+
+$('#collapsingLeftSideBar').click(function() {
+    let leftSideBar = $('#sidebar');
+    leftSideBar.css('margin-left') === '-250px' ? leftSideBar.css({"margin-left": '0'}) : leftSideBar.css({"margin-left": '-250px'});
+});
--- public/assets/js/left-side-bar.js
+++
@@ -1,4 +0,0 @@
-$('#collapsingLeftSideBar').click(function() {
-    let leftSideBar = $('#sidebar');
-    leftSideBar.css('margin-left') === '-250px' ? leftSideBar.css({"margin-left": '0'}) : leftSideBar.css({"margin-left": '-250px'});
-});
